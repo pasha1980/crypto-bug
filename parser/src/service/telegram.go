f50f2f997c4e0069d33c046a1b9f9213f0a25077
pashka <hvalyginpavel@gmail.com>
2022-02-18 18:59:08 +0200

    telegram service

---
+++ parser/src/service/telegram.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"bytes"
+	rootConfig "crypto-bug/config"
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type TelegramMessage struct {
+	Text   string
+	ChatId string
+}
+
+type TelegramResponse struct {
+	Description string `json:"description"`
+}
+
+func (message TelegramMessage) Send() {
+	var response TelegramResponse
+
+	client := rootConfig.Client
+	token := os.Getenv("TELEGRAM_BOT_TOKEN")
+
+	body, _ := json.Marshal(message)
+	responseRaw, err := client.Post(
+		"https://api.telegram.org/bot"+token+"/sendMessage",
+		"application/json",
+		bytes.NewReader(body),
+	)
+	if err != nil {
+		log.Println("Sending message error. Message: " + err.Error())
+		return
+	}
+
+	defer responseRaw.Body.Close()
+	_ = json.NewDecoder(responseRaw.Body).Decode(&response)
+	if responseRaw.StatusCode != 200 {
+		log.Println("Telegram return non 200 response. Message: " + response.Description)
+	}
+}
+
+func (message TelegramMessage) SendToMe() {
+	myChatId := os.Getenv("TELEGRAM_MY_CHAT_ID")
+	message.ChatId = myChatId
+	message.Send()
+}
