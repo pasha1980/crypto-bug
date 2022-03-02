e31fe9b09bb9dc5511b3c0f54f7f4b2668cda361
pashka <hvalyginpavel@gmail.com>
2022-03-02 18:27:22 +0200

    fix extra.log path

--- parser/src/service/logger.go
+++ parser/src/service/logger.go
@@ -21,7 +21,7 @@ func Log(message string, logType string) {
 }
 
 func ExtraLog(message string) {
-	f, err := os.OpenFile("extra.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	f, err := os.OpenFile("/extra.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		return // todo
 	}
